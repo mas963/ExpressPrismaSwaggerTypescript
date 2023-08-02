0459bdd1735fce7e7b8a6a1cf6c57639fced8c48
Yasar Can <38841941+mas963@users.noreply.github.com>
2023-08-02 21:01:55 +0300

    first comiit

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import { PrismaClient } from "@prisma/client"
+import express from "express"
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+//Swagger Integration
+var swaggerUi = require('swagger-ui-express');
+
+var swaggerDocument = require('./swagger.json');
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// ... your RES API routes will go here
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
+
+app.get('/feed', async (req, res) => {
+    const posts = await prisma.post.findMany({
+        where: { published: true },
+        include: { author: true }
+    })
+    res.json(posts)
+})
+
+app.get(`/post/:id`, async (req, res) => {
+    const { id } = req.params
+    const post = await prisma.post.findUnique({
+        where: { id: Number(id) },
+    })
+    res.json(post)
+})
+
+app.post(`/user`, async (req, res) => {
+    const result = await prisma.user.create({
+        data: { ...req.body },
+    })
+    res.json(result)
+})
+
+app.post(`/post`, async (req, res) => {
+    const { title, content, authorEmail } = req.body
+    const result = await prisma.post.create({
+        data: {
+            title,
+            content,
+            published: false,
+            author: { connect: { email: authorEmail } },
+        },
+    })
+    res.json(result)
+})
+
+app.put('/post/publish/:id', async (req, res) => {
+    const { id } = req.params
+    const post = await prisma.post.update({
+        where: { id: Number(id) },
+        data: { published: true },
+    })
+    res.json(post)
+})
+
+app.delete(`/post/:id`, async (req, res) => {
+    const { id } = req.params
+    const post = await prisma.post.delete({
+        where: { id: Number(id) },
+    })
+    res.json(post)
+})
+
+app.listen(3000, () =>
+    console.log('REST API server ready at: http://localhost:3000'),
+)
